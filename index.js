c660f23ec0d76b9a4213040e3d2e2687d30a3636
NicoleStrunga <nicole.strungova@gmail.com>
2021-04-24 15:27:47 +0200

    Pridani animace, porad fuckup se yobrayovanim symbolu

--- index.js
+++ index.js
@@ -2,14 +2,19 @@
 
 let player = 'circle';
 
+const symbol = document.querySelector('.icon_symbol');
+
 const addSymbol = (event) => {
   if (player === 'circle') {
     event.target.classList.add('board__field--circle');
     player = 'cross';
+    symbol.src = 'Piskvorky_rules/cross.svg';
   } else {
     event.target.classList.add('board__field--cross');
     player = 'circle';
+    symbol.src = 'Piskvorky_rules/circle.svg';
   }
+  event.target.disabled = true;
 };
 
 const fieldElm = document.querySelectorAll('.policko');
