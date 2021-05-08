1b094215e34ab5fbaaf12cc8d105c8b62f529c36
NicoleStrunga <nicole.strungova@gmail.com>
2021-05-08 19:34:53 +0200

    Funtions getSymbol, getField and getPosition are done.

--- index.js
+++ index.js
@@ -15,9 +15,34 @@ const addSymbol = (event) => {
     symbol.src = 'Piskvorky_rules/circle.svg';
   }
   event.target.disabled = true;
+  isWinningMove(event.target);
 };
 
 const fieldElm = document.querySelectorAll('.policko');
 for (let i = 0; i < fieldElm.length; i++) {
   fieldElm[i].addEventListener('click', addSymbol);
 }
+
+const getSymbol = (fieldElm) => {
+  if (fieldElm.classList.contains('board__field--circle')) {
+    return 'circle';
+  } else if (fieldElm.classList.contains('board__field--cross')) {
+    return 'cross';
+  }
+};
+
+const boardSize = 10;
+const fields = document.querySelectorAll('.policko');
+
+const getField = (row, column) => fieldElm[row * boardSize + column];
+
+const getPosition = (fieldElm) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && btnsElm !== fields[fieldIndex]) {
+    fieldIndex += 1;
+  }
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
